b14793136302e8cb3967077aeae710d6c0d39a09
tkurudi <t.kurudi209@gmail.com>
2019-09-03 17:53:18 +1200

    new

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,14 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hell World");
+        System.out.println("Hello Name");
+
+        int myFirstNumber = 35;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+
+        System.out.println(myTotal);
     }
 
 }
